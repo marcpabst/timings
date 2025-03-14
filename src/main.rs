c051920465e0ff9704e1337887f338ceae33617b
Marc Pabst <mail@xxxxxxx>
2025-03-14 20:33:54 +0000

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::borrow::Cow;
+use polars::prelude::*;
+use std::{borrow::Cow, collections::HashMap, fs::File, sync::Mutex};
 use wgpu::hal::Adapter;
 use winit::{
     event::{Event, WindowEvent},
@@ -7,6 +8,33 @@ use winit::{
 };
 
 use serialport::SerialPort;
+
+#[derive(Debug, Clone)]
+struct VBlankRecord {
+    timestamp: i64,
+    count: i64,
+    event_type: String,
+}
+
+macro_rules! struct_to_dataframe {
+    ($input:expr, [$($field:ident),+]) => {
+        {
+            let len = $input.len().to_owned();
+
+            // Extract the field values into separate vectors
+            $(let mut $field = Vec::with_capacity(len);)*
+
+            for e in $input.into_iter() {
+                $($field.push(e.$field);)*
+            }
+            df! {
+                $(stringify!($field) => $field,)*
+            }
+        }
+    };
+}
+
+const COLLECT_FRAMES: i64 = 1000;
 
 use windows::Win32::Graphics::Dxgi::DXGI_FRAME_STATISTICS;
 
@@ -19,7 +47,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         backends: wgpu::Backends::DX12,
         ..Default::default()
     };
-    let instance = wgpu::Instance::new(instance_desc);
+    let instance = wgpu::Instance::new(&instance_desc);
 
     let mut surface = instance.create_surface(&window).unwrap();
     let adapter = instance
@@ -34,17 +62,15 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
     // Create the logical device and command queue
     let (device, queue) = adapter
-        .request_device(
-            &wgpu::DeviceDescriptor {
-                label: None,
-                required_features: wgpu::Features::empty(),
-                // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
-                required_limits: wgpu::Limits::downlevel_webgl2_defaults()
-                    .using_resolution(adapter.limits()),
-                memory_hints: wgpu::MemoryHints::MemoryUsage,
-            },
-            None,
-        )
+        .request_device(&wgpu::DeviceDescriptor {
+            label: None,
+            required_features: wgpu::Features::empty(),
+            // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
+            required_limits: wgpu::Limits::downlevel_webgl2_defaults()
+                .using_resolution(adapter.limits()),
+            memory_hints: wgpu::MemoryHints::MemoryUsage,
+            trace: wgpu::Trace::Off,
+        })
         .await
         .expect("Failed to create device");
 
@@ -93,29 +119,31 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
     surface.configure(&device, &config);
 
     unsafe {
-        surface.as_hal::<wgpu::core::api::Dx12,_,_>(
-            |surface| {
-
-                let sc = surface.unwrap().swap_chain();
-                let sc = sc.as_ref().unwrap();
-                let sc2 = sc.raw_swap_chain();
-                sc2.SetFullscreenState(true, None)
-            }
-        )
-    };
+        surface.as_hal::<wgpu::hal::api::Dx12, _, _>(|surface| {
+            let sc = surface.unwrap().swap_chain().read();
+            let sc = sc.as_ref().unwrap().as_raw();
+            sc.SetMaximumFrameLatency(1).unwrap();
+        });
+    }
 
     // open serial port
-    let mut port = serialport::new("COM3", 115200)
-        .timeout(std::time::Duration::from_millis(100))
-        .data_bits(serialport::DataBits::Eight)
-        .flow_control(serialport::FlowControl::None)
-        .open()
-        .expect("Failed to open serial port");
+    // let mut port = serialport::new("COM3", 115200)
+    //     .timeout(std::time::Duration::from_millis(100))
+    //     .data_bits(serialport::DataBits::Eight)
+    //     .flow_control(serialport::FlowControl::None)
+    //     .open()
+    //     .expect("Failed to open serial port");\
 
-    let mut last_time = 0;
+    let records = Arc::new(Mutex::new(Vec::new()));
+
+    // use QueryPerformanceCounter
+    let win_start = get_qpc_timestamp().unwrap();
+
+    // create Instant from QueryPerformanceCounter
+    let cpu_start = std::time::Instant::now();
+
+    let mut running_frame = 0;
     let mut last_frame = 0;
-    let mut last_present_count = 0;
-    let mut running_frame: u8 = 0;
 
     let window = &window;
     event_loop
@@ -140,8 +168,6 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                         window.request_redraw();
                     }
                     WindowEvent::RedrawRequested => {
-
-
                         let frame = surface
                             .get_current_texture()
                             .expect("Failed to acquire next swap chain texture");
@@ -169,14 +195,11 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                                     occlusion_query_set: None,
                                 });
                             rpass.set_pipeline(&render_pipeline);
-                            if running_frame > 10 {
-                              // do nothing
-                            } else if running_frame % 2 == 0 {
+                            if running_frame % 2 == 0 {
                                 rpass.draw(0..6, 0..1);
                             } else {
                                 // do nothing
                             }
-
                         }
 
                         queue.submit(Some(encoder.finish()));
@@ -189,33 +212,43 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                         while present_stats.PresentCount == last_frame {
                             present_stats = get_frame_stats(&surface);
                             // sleep for 1us
-                            std::thread::sleep(std::time::Duration::from_micros(1));
+                            // std::thread::sleep(std::time::Duration::from_micros(1));
                         }
 
-
-                        // convert to bytes running_frame in
-                        if running_frame <= 10 {
-                            let payload = (running_frame + 1).to_be_bytes();
-                            port.write(&payload).expect("Failed to write to serial port");
-                            port.flush().expect("Failed to flush serial port");
-                        }
-
-                        let frame_diff = present_stats.PresentCount- last_frame;
-                        let time_diff = present_stats.SyncQPCTime - last_time;
-
-                        if frame_diff > 1 {
-                            println!("Missed Frames: {}", frame_diff - 1);
-                        }
-
-                        println!("Frame Time: {}ms (for {} frames)", time_diff as f64 / 10_000.0, frame_diff);
-                        println!("Present Count: {}", present_stats.PresentRefreshCount - last_present_count);
-
-
-                        last_time = present_stats.SyncQPCTime;
                         last_frame = present_stats.PresentCount;
-                        last_present_count = present_stats.PresentRefreshCount;
-                        running_frame = (last_frame % 20) as u8;
 
+                        // get frame stats timestamp
+                        let fs_timestamp = present_stats.SyncQPCTime - win_start;
+
+                        // take cpu timestamp
+                        let cpu_timestamp = (cpu_start.elapsed().as_nanos() / 100) as i64;
+
+                        records.lock().unwrap().push(VBlankRecord {
+                            timestamp: fs_timestamp,
+                            count: present_stats.PresentRefreshCount as i64,
+                            event_type: "sync_qpc_time".to_string(),
+                        });
+
+                        records.lock().unwrap().push(VBlankRecord {
+                            timestamp: cpu_timestamp,
+                            count: present_stats.PresentRefreshCount as i64,
+                            event_type: "cpu_time".to_string(),
+                        });
+
+                        running_frame = running_frame + 1;
+                        println!("Collecting frame: {} / {}", running_frame, COLLECT_FRAMES);
+
+                        if running_frame > COLLECT_FRAMES {
+                            // write to csv
+                            let mut df = struct_to_dataframe!(
+                                records.lock().unwrap().clone(),
+                                [timestamp, count, event_type]
+                            )
+                            .unwrap();
+                            write_df_csv(&mut df).unwrap();
+
+                            target.exit();
+                        }
                     }
                     WindowEvent::CloseRequested => target.exit(),
                     _ => {}
@@ -229,15 +262,11 @@ pub fn get_frame_stats(surface: &wgpu::Surface) -> DXGI_FRAME_STATISTICS {
     let mut present_stats: DXGI_FRAME_STATISTICS = DXGI_FRAME_STATISTICS::default();
 
     unsafe {
-        surface.as_hal::<wgpu::core::api::Dx12,_,_>(
-            |surface| {
-
-                let sc = surface.unwrap().swap_chain();
-                let sc = sc.as_ref().unwrap();
-                let sc2 = sc.raw_swap_chain();
-                sc2.GetFrameStatistics(&mut present_stats)
-            }
-        )
+        surface.as_hal::<wgpu::hal::api::Dx12, _, _>(|surface| {
+            let sc = surface.unwrap().swap_chain().read();
+            let sc = sc.as_ref().unwrap().as_raw();
+            sc.GetFrameStatistics(&mut present_stats);
+        })
     };
 
     present_stats
@@ -245,10 +274,18 @@ pub fn get_frame_stats(surface: &wgpu::Surface) -> DXGI_FRAME_STATISTICS {
 pub fn main() {
     let event_loop = EventLoop::new().unwrap();
     #[allow(unused_mut)]
-    let primary_monitor  = event_loop.available_monitors().last().expect("Failed to get primary monitor");
-    let video_mode = primary_monitor.video_modes().next().expect("Failed to get video mode");
+    let primary_monitor = event_loop
+        .available_monitors()
+        .last()
+        .expect("Failed to get primary monitor");
+    let video_mode = primary_monitor
+        .video_modes()
+        .next()
+        .expect("Failed to get video mode");
     println!("Video mode: {:?}", video_mode);
-    let mut builder = winit::window::WindowBuilder::new().with_fullscreen(Some(winit::window::Fullscreen::Borderless(Some(primary_monitor))));
+    let mut builder = winit::window::WindowBuilder::new().with_fullscreen(Some(
+        winit::window::Fullscreen::Borderless(Some(primary_monitor)),
+    ));
     #[cfg(target_arch = "wasm32")]
     {
         use wasm_bindgen::JsCast;
@@ -276,4 +313,20 @@ pub fn main() {
         console_log::init().expect("could not initialize logger");
         wasm_bindgen_futures::spawn_local(run(event_loop, window));
     }
+}
+
+pub fn get_qpc_timestamp() -> windows::core::Result<i64> {
+    let mut timestamp: i64 = 0;
+    unsafe {
+        windows::Win32::System::Performance::QueryPerformanceCounter(&mut timestamp)?;
+    }
+    Ok(timestamp)
+}
+fn write_df_csv(df: &mut DataFrame) -> PolarsResult<()> {
+    let mut file = File::create("example.csv").expect("could not create file");
+
+    CsvWriter::new(&mut file)
+        .include_header(true)
+        .with_separator(b',')
+        .finish(df)
 }
